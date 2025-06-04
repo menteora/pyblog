5ae1e49dec41f74034b823d224b3a262c6a1994d
menteora <menteora@gmail.com>
2025-06-04 16:48:05 +0200

    addede hero-image

--- run_tests.py
+++ run_tests.py
@@ -8,12 +8,12 @@ import generate_site
 
 def main():
     # run core tests
-    subprocess.check_call(['pytest', 'tests'])
+    subprocess.check_call(['pytest', '-v', 'tests'])
     # run tests for each active plugin
     for name in generate_site.PLUGINS:
         plugin_tests = Path('plugins') / name / 'tests'
         if plugin_tests.is_dir():
-            subprocess.check_call(['pytest', str(plugin_tests)])
+            subprocess.check_call(['pytest', '-v', str(plugin_tests)])
 
 
 if __name__ == '__main__':
